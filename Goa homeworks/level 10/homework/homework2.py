fa3339d5ba707ca0f92b509e6aef8249e8f3c2e2
DemetreKhosro <demetrexosroshvili@gmail.com>
2025-03-12 00:00:35 +0400

    second commit

---
+++ Goa homeworks/level 10/homework/homework2.py
@@ -0,0 +1,2 @@
+name = "Demetre"
+print(type(name))
---
+++ Goa homeworks/level 10/homework/homework5.py
@@ -0,0 +1,6 @@
+#მომხმარებელს შემოაქვს ინფორმაცია
+age = int(input("Enter your age: "))
+years = int(input("Enter the amount of years: "))
+#ახლა გამოვთვლით თუ რამდენი წლის იქნება მომხმარებელი მისი შემოყვანილი წლების რაოდენობის შემდეგ ცვლადი EA-თი (Estimated Age)
+EA = age + years
+print(str(years) + " " + "წლის შემდეგ, თქვენ იქნებით" + " " + str(EA) + " " + "წლის")
