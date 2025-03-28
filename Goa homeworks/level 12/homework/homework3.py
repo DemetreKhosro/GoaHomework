b22cb97b01dd930cebc1b519d25339b4c374cf8f
DemetreKhosro <demetrexosroshvili@gmail.com>
2025-03-28 16:28:36 +0400

    second commit

--- Goa homeworks/level 12/homework/homework3.py
+++ Goa homeworks/level 12/homework/homework3.py
@@ -2,5 +2,6 @@ operation1 = 5 > 3 # True
 operation2 = 785 < 113 # False
 operation3 = 42 != "apple" # True
 operation4 = 2 >= 4 # False
-operation5 = 6 <= 7
-print(operation5)
+operation5 = 6 <= 7 # True
+
+#ოპერატორი და ოპერაცია განსხვავდება იმით რომ ოპერატორი არის ოპერაციის შემსრულებელი ნიშანი ხოლო ოპერაცია შესრულებული მაგალითი.
--- Goa homeworks/level 12/homework/homework4.py
+++ Goa homeworks/level 12/homework/homework4.py
@@ -1,19 +1,24 @@
+# var1
 temperature = 36
 sunny = True
 var1 = (temperature > 35 and sunny) # True
-print(var1)
 
+# var2
 temp = 13
 time = "night"
 var2 = (temp > 20 and not time) # False
-print(var2)
 
+# var3
 kids = 5
 game = 7
 var3 = (kids <= 7 or game) # True
-print(var3)
 
+# var4
 money = 10
 food = 15
-var4 = (money <= 15 or food)
-print(var4)
+var4 = (money <= 15 or food) # True
+
+# var5
+temperature = 25
+rainy = True
+var5 = (temperature > 30 and not rainy)  # False
