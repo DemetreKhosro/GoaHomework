cc9a744d89b237da2a5863449000fe8cc27cc1b0
DemetreKhosro <demetrexosroshvili@gmail.com>
2025-04-07 22:06:10 +0400

    second commit

--- Goa homeworks/level 16/homework/BonusHomework14.py
+++ Goa homeworks/level 16/homework/BonusHomework14.py
@@ -4,5 +4,5 @@ guess = " "
 while guess != pin:
     guess = input("Enter the pin: ")
     attempts += 1
-print("It took you " + str(attempts) + " " + "tries to log in to your account!")
 print("Correct!")
+print("It took you " + str(attempts) + " " + "tries to log in to your account!")
--- Goa homeworks/level 16/homework/homework2.py
+++ Goa homeworks/level 16/homework/homework2.py
@@ -1,4 +1,4 @@
-sum  = 0
+sum = 0
 for number in range(20, 40):
     sum += number
 print(sum)
