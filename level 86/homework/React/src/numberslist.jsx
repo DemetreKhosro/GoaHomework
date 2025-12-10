0795fa93add3604d53647a056d66f2db15cffcfa
DemetreKhosro <demetrexosroshvili@gmail.com>
2025-12-10 20:04:38 +0400

    second commit

---
+++ level 86/homework/React/src/numberslist.jsx
@@ -0,0 +1,11 @@
+function NumberList() {
+    return (
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+        </ul>
+    )
+}
+
+export default NumberList
