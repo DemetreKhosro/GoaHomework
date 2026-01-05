b4fb050f9f8dfc99797200c10eefa7bee6004d2b
DemetreKhosro <demetrexosroshvili@gmail.com>
2026-01-05 14:52:16 +0400

    Add files via upload

---
+++ level 92/classwork/cw4.py
@@ -0,0 +1,17 @@
+''' შექმენით ფუქნცია სახელად case_swap რომელიც მიიღებს სიტყვას, ფუნქციამ ყველა დიდი ანბანის ასო უნდა გარდაქმნას პატარად და პირიქით, ხოლო სხვა სახის სიმბოლები იგივე უნდა დარჩეს (.upper(), .lower() მეთოდების გამოყენების გარეშე) '''
+
+string = 'DemEtRE'
+
+def case_swap(text):
+  result = ''
+  for ch in text:
+    code = ord(ch)
+    if 65 <= code <= 90:
+      result += chr(code + 32)
+    elif 97 <= code <= 122:
+      result += chr(code - 32)
+    else:
+      result += ch
+  return result
+
+print(case_swap(string))
